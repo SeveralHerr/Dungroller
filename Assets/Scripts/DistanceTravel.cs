72008dfe4ecddfb8297484d1ed91ae97b02096f1
entoke <attackchelseagrin@gmail.com>
2021-11-22 22:08:30 -0500

    added timer

---
+++ Assets/Scripts/DistanceTravel.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DistinceTravel : MonoBehaviour
+{
+    private float currentTimer;
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentTimer = Time.fixedTime;
+
+    }
+}
