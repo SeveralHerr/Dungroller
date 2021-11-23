45d99ceb2192e4fa6265e871aad9fb5bd29a0a7a
James Herr <several.herr@gmail.com>
2021-11-22 21:16:23 -0600

    Added Manager

--- Assets/Scripts/HandDropper.cs
+++ Assets/Scripts/HandDropper.cs
@@ -1,4 +1,5 @@
 using System.Collections;
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,7 +7,6 @@ public class HandDropper : MonoBehaviour
 {
     private Rigidbody2D rigidbody2d;
     private Vector3 targetlocation;
-    public Transform playertransform;
 
     public GameObject shadow;
 
@@ -15,7 +15,8 @@ public class HandDropper : MonoBehaviour
     void Start()
     {
         rigidbody2d = GetComponent<Rigidbody2D>();
-        targetlocation = playertransform.position;
+        targetlocation = Player.Instance.transform.position;
+        Debug.Log(Player.Instance.transform.position.x + "," + Player.Instance.transform.position.y);
         var initialhandpos = new Vector3(targetlocation.x + 3, 17);
         transform.position = initialhandpos;
 
@@ -24,33 +25,38 @@ public class HandDropper : MonoBehaviour
     }
     private void Update()
     {
-        if(transform.position.y <= 3.3f && !wasHandDown)
+        if (transform.position.y <= 3.3f && !wasHandDown)
         {
             wasHandDown = true;
             return;
         }
 
-        if(wasHandDown)
+        if (wasHandDown)
         {
             // fuck you
             var movementSpeed = 0.2f;
-            var newPos = new Vector2(transform.position.x , transform.position.y + movementSpeed);
+            var newPos = new Vector2(transform.position.x, transform.position.y + movementSpeed);
             rigidbody2d.MovePosition(newPos);
+
+            if (rigidbody2d.position.y >= 17 && wasHandDown)
+            {
+                var shdw = GameObject.FindGameObjectWithTag("Shadow");
+                Destroy(shdw);
+                Destroy(gameObject);
+            }
 
             return;
         }
-
-        var newPosition = new Vector2(transform.position.x , transform.position.y -.2f);
+       
+        var newPosition = new Vector2(transform.position.x, transform.position.y - .2f);
         rigidbody2d.MovePosition(newPosition);
-
-
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(Player.Instance.HitPlayer(collision.tag))
         {
-            Destroy(shadow.gameObject);
+           
             Player.Instance.DestroyPlayer();
         }
     }
