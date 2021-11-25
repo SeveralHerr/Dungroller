82bbc3f17d96d47c37831e1abb92871e039c7690
James Herr <several.herr@gmail.com>
2021-11-24 21:38:29 -0600

    Added Background

---
+++ Assets/Scripts/Background.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Background : MonoBehaviour
+{
+    private Rigidbody2D rigidbody2d;
+    public GameObject background;
+    private bool hasMadeNewBackground = false;
+    public float speedModifier = 1;
+
+    private void Start()
+    {
+        rigidbody2d = GetComponent<Rigidbody2D>();
+    }
+    private void FixedUpdate()
+    {
+        var newPosition = new Vector2(transform.position.x - (Settings.Instance.GlobalMoveSpeed * speedModifier), transform.position.y);
+        rigidbody2d.MovePosition(newPosition);
+
+        if (transform.position.x <= -8 && !hasMadeNewBackground)
+        {
+            var newBackgroundPosition = new Vector2(31, transform.position.y);
+            Instantiate(background, newBackgroundPosition, Quaternion.identity);
+            hasMadeNewBackground = true;
+
+        }
+
+        if (transform.position.x <= -33 )
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Settings.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Settings : SingletonMonobehavior<Settings>
+{
+    public float GlobalMoveSpeed = 0.1f;
+}
